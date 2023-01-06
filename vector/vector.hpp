64125f1e067e35cde1e62232818b5b5e987366e7
dependentmadani <madani.badaoui12@gmail.com>
2023-01-06 22:41:29 +0100

    finish with vector member functions prototypes

--- vector/vector.hpp
+++ vector/vector.hpp
@@ -55,9 +55,86 @@ namespace ft
             // destructs the vector
             ~vector();
 
+            vector& operator= (const vector& other );
             
+            iterator begin();
+            const_iterator begin() const;
+            iterator end();
+            const_iterator end() const;
+            reverse_iterator rbegin();
+            const_reverse_iterator rbegin() const;
+            reverse_iterator rend();
+            const_reverse_iterator rend() const;
 
+            void assign( size_type count, const T& value);
+            template<class InputIt>
+            void assign( InputIt first, InputIt last);
+
+            allocator_type get_allocator() const;
+
+            bool empty() const;
+
+            size_type size() const;
+            size_type max_size() const;
+
+            void reserve( size_type new_cap );
+            size_type capacity() const;
+
+            void clear();
+
+            //insert value before pos, It returns iterator pointing to the inserted value
+            iterator insert ( const_iterator pos, const T& value );
+            //insert count copies of the value before pos, it returns iterator pointing to the first element
+            //inserted, or pos if count == 0.
+            iterator insert ( const_iterator pos, size_type count, const T& value);
+            //insert elements from range [first,last) before pos
+            //this overload has the same effect as ovverload of previous function if InputIt is an intergral type.
+            //The behavior is undefined if first and last are iterators into *this.
+            //It returns iteratorpoiting to the first element inserted, or pos if first == last.
+            template<class InputIt>
+            iterator insert ( const_iterator pos, InputIt firstm InputUt last );
+
+            //removes the element at pos
+            iterator erase( iterator pos );
+            //removes the elements in the range [first, last)
+            iterator erase( iterator first , iterator last );
+
+            void push_back( const T& value );
+
+            void pop_back();
+
+            //resize the container to contain count elements.
+            void resize( size_type count, T value = T() );
+
+            void swap ( vector& other );
     };
+
+    template<class T , class Alloc>
+    inline bool operator== (const ft::vector<T, Alloc>& lhs,
+                            const ft::vector<T,Alloc>&rhs);
+
+    template<class T, class Alloc>
+    inline bool operator!= (const ft::vector<T, Alloc>& lhs,
+                            const ft::vector<T,Alloc>&rhs);
+    
+    template<class T, class Alloc>
+    inline bool operator< (const ft::vector<T, Alloc>& lhs,
+                            const ft::vector<T,Alloc>&rhs);
+    
+    template<class T, class Alloc>
+    inline bool operator<= (const ft::vector<T, Alloc>& lhs,
+                            const ft::vector<T,Alloc>&rhs);
+    
+    template<class T, class Alloc>
+    inline bool operator> (const ft::vector<T, Alloc>& lhs,
+                            const ft::vector<T,Alloc>&rhs);
+
+    template<class T, class Alloc>
+    inline bool operator>= (const ft::vector<T, Alloc>& lhs,
+                            const ft::vector<T,Alloc>&rhs);
+    
+    template<class T, class Alloc>
+    inline void swap (ft::vector<T, Alloc>& lhs, ft::vector<T, Alloc>& rhs);
 }
 
 #include <vector>
