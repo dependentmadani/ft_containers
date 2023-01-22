41e5ba6ecb925a9459e2c52529e1c12570926f47
dependentmadani <madani.badaoui12@gmail.com>
2023-01-22 02:50:16 +0100

    working on insertion of nodes in tree using avl

--- AVL.hpp
+++ AVL.hpp
@@ -1,8 +1,10 @@
 #ifndef AVL_HPP
 # define AVL_HPP
 
+# include "utility.hpp"
 # include <memory>
 # include <functional>
+# include <algorithm>
 
 namespace ft
 {
@@ -10,10 +12,11 @@ namespace ft
     struct AVL
     {
         Pair value;
-        AVL* root;
-        AVL* left;
-        AVL* right;
+        AVL* parent;
+        AVL* left_child;
+        AVL* right_child;
         int height;
+        int balanced_factor;
         AVL (Pair& other): value(other) {};
     };
 
@@ -43,10 +46,105 @@ namespace ft
                 _compare = other._compare;
                 _allocator_node = other._allocator_node;
                 _allocator = other._allocator;
-                insert_node(other.root);
+                insert_all_nodes(other.root);
                 return (*this);
             }
 
+            // possibility to insert a tree in a tree
+            //this function insert all the nodes in the root
+            void insert_all_nodes(node_type *node)
+            {
+                if (node != NULL)
+                    insertion(node->value);
+                    if (node->left_child != NULL)
+                        insert_all_nodes(node->left_child);
+                    else if (node->right_child != NULL)
+                        insert_all_nodes(node->right_child);
+            }
+
+            // this function takes the values of a type and insert it the root
+            node_type* insertion(T value)
+            {
+                if (!available(root, value))
+                {
+                    root = insert(root, value);
+                    node_number++;
+                    return root;
+                }
+                return NULL;
+            }
+
+            //this function checks if a value of type T does exist in the tree or not
+            bool available_in_tree(T value) const
+            {
+                return available_in_tree(root, value);
+            }
+
+            //this function checks if a value of type key_value does exist in the tree or not
+            bool available_in_tree(key_type value) const
+            {
+                return available_in_tree(root, value);
+            }
+
+        private:
+            //this private function checks if a value of type T does exist in the tree or not
+            bool available_in_tree(node_type* node, T value) const
+            {
+                if (node != NULL)
+                    return false;
+                if (node->value.first == value.first)
+                    return true;
+                if (_compare(value.first, node->value.first) == true)
+                    return available_in_tree(node->left_child, value);
+                if (_compare(value.first, node->value.first) == false)
+                    return available_in_tree(node->right_child, value);
+                return true;
+            }
+
+            //this function checks if a value of type key_type does exist in the tree or not
+            bool available_in_tree(node_type* node, key_type value) const
+            {
+                if (node != NULL)
+                    return false;
+                if (node->value.first == value.first)
+                    return true;
+                if (_compare(value.first, node->value.first) == true)
+                    return available_in_tree(node->left_child, value);
+                if (_compare(value.first, node->value.first) == false)
+                    return available_in_tree(node->right_child, value);
+                return true;
+            }
+        
+            //this private function is used to insert a value in root node, which is used
+            // by the public insertion function.
+            node_type* insertion(node_type* node, T value)
+            {
+                if (node == NULL)
+                {
+                    root = _allocator_node.allocate(1);
+                    _allocator.construct(&root->value, value);
+                    root->height = 0;
+                    root->balanced_factor = 0;
+                    root->left_child = NULL;
+                    root->right_child = NULL;
+                    root->parent = NULL;
+                    return (root);
+                }
+                if (_compare(value.first, node->value.first) == true)
+                {
+                    node_type* left_node = insertion(node->left_child, value);
+                    node->left_child = left_node;
+                    left_node->parent = node; // unuseful
+                }
+                else
+                {
+                    node_type* right_node = insertion(node->right_child, value);
+                    node->right_child = right_node;
+                    right_node->parent = node; // unuseful
+                }
+                update_bf_height(node);
+                return balance_tree(node);
+            }
             
     };
 
